8e708b8eee9640fdb59d7b157706a571b20fefb4
Shantanu Gondane <113772055+shantanugondane@users.noreply.github.com>
2022-12-25 01:44:35 +0530

    title__workdone

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { images } from '../../constants';
+import { SubHeading } from '../../components'
 
 import './Header.css';
 
 const Header = () => (
-  <div className='app__header app__wrapper section__padding' id=''>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className='app__header-h1'>The key to Fine Dining</h1>
+    </div> 
+    <div className="app__wrapper_img">
+
+    </div>
   </div>
 );
 
