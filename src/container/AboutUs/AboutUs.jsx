9b509eb57511714410e0b1dceaa8eb6d43e1eba4
Shantanu Gondane <113772055+shantanugondane@users.noreply.github.com>
2023-02-02 01:55:34 +0530

    header page done
    started with the about us page

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import {images} from "../../constants"
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    <div className="app__aboutus-overlay flex__center">
+<img src={images.G} alt='g letter' />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="spoon"  className="spoon__img"/>
+        <p className="p__opensans">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam aut qui maxime quasi eos dolore nobis veniam aperiam illum tempora. Adipisci tempora, repellat nisi laudantium sequi provident ipsam non dolorum.</p>
+        <button type="button" className="custom__button">Know more</button>
+              </div>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -10,9 +10,10 @@ const Header = () => (
       <SubHeading title="Chase the new flavour" />
       <h1 className='app__header-h1'>The key to Fine Dining</h1>
       <p className='p__opensans' style={{margin:'2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus</p>
+      <button type="button" className="custom__button">Explore Menu</button>
     </div> 
     <div className="app__wrapper_img">
-
+      <img src={images.welcome} alt="header img" />
     </div>
   </div>
 );
