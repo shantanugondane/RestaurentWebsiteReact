bf63d28fcd795ead9ccec3377ab7431394d63ede
Shantanu Gondane <113772055+shantanugondane@users.noreply.github.com>
2023-02-15 01:34:26 +0530

    updatedwine -section

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -7,8 +7,17 @@ const SpecialMenu = () => (
   <div className='app__specialMenu flex__center section__padding' id='menu'>
     <div className="app__specialMenu-title">
       <SubHeading title="Menu that feets you palatte"/>
+      <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
-    
+    <div className="app__specialMenu-menu">
+      <div className="app_specialMenu-menu_wine flex__Center">
+        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+        <div className="app_speacialMenu_menu_item "></div>
+        {data.wines.map((wine, index)=>(
+          <p>{wine.title}</p>
+        ))}
+      </div>
+    </div>
   </div>
 );
 
